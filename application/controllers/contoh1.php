747d7a650cd6e35be67f75f53fdcb7faae15d232
High-cal <h41kal.pr@gmail.com>
2023-10-19 15:46:40 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Imam Nawawi 
+        Saya Tinggal Di Ciputat 
+        Olahraga Yang Saya Sukai Adalah Bulutangkis";
+    }
+}
